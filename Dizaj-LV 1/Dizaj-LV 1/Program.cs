891673d6a48fd74f2b0218ea13a1a292a52948b9
Andrija Dujak <and123dk@gmail.com>
2020-04-23 13:22:45 +0200

    example project added

---
+++ Dizaj-LV 1/Dizaj-LV 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dizaj_LV_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
